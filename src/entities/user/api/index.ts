502eb7ff425879f75f4bd9dc9c1504573efc4b7c
Bumpist <104641096+parksubeom@users.noreply.github.com>
2025-12-12 01:00:52 +0900

    build : 빌드 수정

--- src/entities/user/api/index.ts
+++ src/entities/user/api/index.ts
@@ -1,13 +1,17 @@
 import { User, UserResponse } from "../model/types"
 
+// ✅ 배포 환경 대응 로직 추가
+const isDev = import.meta.env.DEV
+const BASE_URL = isDev ? "/api" : "https://dummyjson.com"
+
 // 기존: 목록 가져오기
 export const getUsers = async (): Promise<UserResponse> => {
-  const response = await fetch("/api/users?limit=0&select=username,image")
+  const response = await fetch(`${BASE_URL}/users?limit=0&select=username,image`)
   return response.json()
 }
 
 // 추가: 특정 유저 상세 정보 가져오기 (ID로 조회)
 export const getUserDetail = async (userId: number): Promise<User> => {
-  const response = await fetch(`/api/users/${userId}`)
+  const response = await fetch(`${BASE_URL}/users/${userId}`)
   return response.json()
 }
